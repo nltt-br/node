8b279faba6097711df21380ccff809d94fc88be3
Negro Largato <nlargato@gmail.com>
2020-05-21 08:50:14 -0300

    v1

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-function soma(n1, n2){
-	return n1 + n2;
-}
+const express = require("express")
+const app = express()
 
-console.log(soma(10,20))
+app.get('/', function(req, res){
+	res.send('Bem vindo ao meu app web')
+})
+
+app.get('/inicio', function(req, res){
+	res.send('Inicio do meu app web')
+})
+
+app.get('/param/:nome', function(req, res){
+	res.send('Seu nome e ' + req.params.nome)
+})
+
+
+app.listen(8081, function(){
+	console.log("Servidor rodando no http://localhost:8081")
+})
